9d35a6c2a408525b19a872526634c281f5725d6d
zuhengming <mingzuheng@126.com>
2020-02-05 14:44:29 +0000

    added the option out_imgfiles in tools/test.py for saving the test labelling images.

--- configs/cascade_rcnn_r50_fpn_1x_fisheye.py
+++ configs/cascade_rcnn_r50_fpn_1x_fisheye.py
@@ -39,7 +39,7 @@ model = dict(
             in_channels=256,
             fc_out_channels=1024,
             roi_feat_size=7,
-            num_classes=81,
+            num_classes= 2, #81,
             target_means=[0., 0., 0., 0.],
             target_stds=[0.1, 0.1, 0.2, 0.2],
             reg_class_agnostic=True,
@@ -52,7 +52,7 @@ model = dict(
             in_channels=256,
             fc_out_channels=1024,
             roi_feat_size=7,
-            num_classes=81,
+            num_classes= 2, #81,
             target_means=[0., 0., 0., 0.],
             target_stds=[0.05, 0.05, 0.1, 0.1],
             reg_class_agnostic=True,
@@ -65,7 +65,7 @@ model = dict(
             in_channels=256,
             fc_out_channels=1024,
             roi_feat_size=7,
-            num_classes=81,
+            num_classes=2, #81,
             target_means=[0., 0., 0., 0.],
             target_stds=[0.033, 0.033, 0.067, 0.067],
             reg_class_agnostic=True,
@@ -164,7 +164,9 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    #dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    #dict(type='Resize', img_scale=(500, 500), keep_ratio=True),
+    dict(type='Resize', img_scale=(1000, 1000), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -175,7 +177,9 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
+        # img_scale=(1333, 800),
+        #img_scale=(500, 500),
+        img_scale=(1000, 1000),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -205,7 +209,8 @@ data = dict(
         img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline))
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+#optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001) ## 8 gpu: lr0.02; 4 gpu: lr 0.01, 1 gpu: lr0.025
+optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001) ## 2 gpu: lr 0.05
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
